a729217369ec1bfd54659d21e8f92b9c27b5dfd2
Arsam-Mughal <mughalarsam42@gmail.com>
2026-01-10 19:13:57 +0100

    added more games tabs + image hover + youtube modal + simple nav-bar

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function openModal(videoId) {
+    const modal = document.getElementById('videoModal');
+    const player = document.getElementById('videoPlayer');
+    player.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    modal.style.display = 'flex';
+}
 
+function closeModal() {
+    const modal = document.getElementById('videoModal');
+    const player = document.getElementById('videoPlayer');
+    player.src = ""; // Stops the video when closed
+    modal.style.display = 'none';
+}
