8022a3eb69d59e917afc99f0f23edf471223ecce
Arsam <mughalarsam42@gmail.com>
2026-07-04 20:51:22 +0500

    added various playable ads

--- game.js
+++ game.js
@@ -1,15 +1,51 @@
 const games = {
-    snake: {
-        title: "Snake",
-        url: "games/snake/index.html"
+    Cowboy_MultiCatch: {
+        title: "Cowboy MultiCatch",
+        url: "Playables/Cowboy_MultiCatch.html"
     },
-    pong: {
-        title: "Pong",
-        url: "games/pong/index.html"
+    Cowboy_LassoMaster: {
+        title: "Cowboy LassoMaster",
+        url: "Playables/Cowboy_LassoMaster.html"
     },
-    flappy: {
-        title: "Flappy Bird",
-        url: "games/flappy/index.html"
+    Jackaroo_KillWin: {
+        title: "Jackaroo Kill to Win",
+        url: "Playables/Jackaroo_KillWin.html"
+    },
+    Jackaroo_SwapMarble: {
+        title: "Jackaroo Swap Marble",
+        url: "Playables/Jackaroo_SwapMarble.html"
+    },
+    PF1_CornV2_Extended: {
+        title: "PF1 CornV2 Extended",
+        url: "Playables/PF1_CornV2_Extended.html"
+    },
+    PF1_MelonV1: {
+        title: "PF1 MelonV1",
+        url: "Playables/PF1_MelonV1.html"
+    },
+    PF2_CornV2_Extended: {
+        title: "PF2 CornV2 Extended",
+        url: "Playables/PF2_CornV2_Extended.html"
+    },
+    PF2_EggsV1: {
+        title: "PF2 EggsV1",
+        url: "Playables/PF2_EggsV1.html"
+    },
+    PF3_TomatoV1: {
+        title: "PF3 TomatoV1",
+        url: "Playables/PF3_TomatoV1.html"
+    },       
+    TP_BumperV1: {
+        title: "TP BumperV1",
+        url: "Playables/TP_BumperV1.html"
+    },      
+    TP_WaterTubeV1: {
+        title: "TP WaterTubeV1",
+        url: "Playables/TP_WaterTubeV1.html"
+    },      
+    Twister_Long: {
+        title: "Twister Long",
+        url: "Playables/Twister_Long.html"
     }
 }
 const params = new URLSearchParams(window.location.search)
